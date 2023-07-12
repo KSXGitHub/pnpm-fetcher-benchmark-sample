a92d523e680bdb62dc6d8d4973b6a26a6d17ec59
khai96_ <hvksmr1996@gmail.com>
2023-07-12 20:20:41 +0700

    Add rs-new

--- members.js
+++ members.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-module.exports = ['rs', 'js'].map(name => {
+module.exports = ['rs-old', 'rs-new', 'js'].map(name => {
   const ownedAssets = path.join(__dirname, 'owned-assets', name)
   const sharedAssets = path.join(__dirname, 'shared-assets')
   const destDir = path.join(__dirname, 'workspaces', name)
